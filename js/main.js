d88fbdf1fd6c70fcc8e84bd8b32367dab9f77dd5
Bruno Campos <brunosopmac@gmail.com>
2018-08-31 18:47:10 -0300

    Criando um template simples para trabalhar os conceitos do WebGL

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 'use strict';
-
-// Renderer
 const [winWidth, winHeight] = [window.innerWidth, window.innerHeight];
-const renderer = new THREE.WebGLRenderer({ alpha: true });
 
+// WebGL Renderer Engine
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(winWidth, winHeight);
 document.body.appendChild(renderer.domElement);
 
-
-// Scene
+// Creating a new Scene
 const scene = new THREE.Scene();
+
+// Perspective Camera
+const camera = new THREE.PerspectiveCamera(45, winWidth / winHeight, 0.1, 1000);
+
+// Positions x,y,z axis of Camera
+camera.position.set(0, 0, 50);
+
+// create the mesh from geometry and material
+/*
+    const mesh = new THREE.Mesh(geometry, material);
+*/
+
+// adding objects to the scene
+/*
+    scene.add(mesh)
+*/
+
+// Rendering the Scene
+const render = () => {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+};
+
+render();
